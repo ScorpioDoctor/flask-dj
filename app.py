f17f663c1a371bf0010f5cac953e50e2a2e37ddb
H_IngJao <605608886@qq.com>
2018-03-20 12:03:00 +0800

    添加fcgi配置
    uwsgi配置也是这么简单

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from flask_dj import FlaskDJ
 from flup.server.fcgi_fork import WSGIServer
 
 app = FlaskDJ(__name__)
-app.jinja_env.auto_reload = True
-
 
 # fcgi部署项目
-#WSGIServer(app, bindAddress="127.0.0.1:9000").run()
+#WSGIServer(app, bindAddress=("127.0.0.1", 5000), multiprocess=True).run()
 
 # 调试项目
-app.run(host="127.0.0.1", port=5000, debug=False)
+app.jinja_env.auto_reload = True
+app.run(host="127.0.0.1", port=5000, debug=True)
 
