107fe7d811e0e8449c502b6d51c099114459cb71
H_IngJao <605608886@qq.com>
2018-03-21 11:31:12 +0800

    Revert "1"
    This reverts commit 24f76f7414b88910ef679491a36437ce43ff85a2.

--- App/Base/utils.py
+++ App/Base/utils.py
@@ -3,7 +3,20 @@
 import requests,random,json
 
 def _sendSms(phone, code):
-    return True
+    content = {
+        "name":"易优贷",
+        "code":code,
+        "minute":"5"}
+    query = 'content=%s&mobile=%s&tNum=T150606060606' % (json.dumps(content),phone)
+    # query = urllib.parse.quote(query)
+    url = 'http://ali-sms.showapi.com/sendSms?%s' % query
+    appcode = '37387f0a1c3045728d3666df9cca8569'
+    headers = {
+        'Authorization':'APPCODE %s' % appcode
+    }
+    r = requests.get(url, headers=headers)
+    return(r.json()['showapi_res_body']['remark'].find('提交成功')>-1)
+
 
 def send_vcode(phone):
     vcode = _generate_vcode()
