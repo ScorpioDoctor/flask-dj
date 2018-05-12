bf2b27de7c7087e3e2dd6ebb40acc8915d1c33c8
H_IngJao <605608886@qq.com>
2018-05-12 19:32:08 +0800

    完善AdminIndex

--- flask_dj/babel/admin/views/index.py
+++ flask_dj/babel/admin/views/index.py
@@ -7,6 +7,25 @@
 from flask_admin import AdminIndexView, expose
 
 class DJAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        """
+            Override this method to add permission checks.
+
+            Flask-Admin does not make any assumptions about the authentication system used in your application, so it is
+            up to you to implement it.
+
+            By default, it will allow access for everyone.
+        """
+        return True
+    def inaccessible_callback(self, name, **kwargs):
+        """
+            Handle the response to inaccessible views.
+
+            By default, it throw HTTP 403 error. Override this method to
+            customize the behaviour.
+        """
+        return abort(403)
+        
     # 返回原始首页
     @expose('/')
     def index(self):
